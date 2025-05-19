0031dc09267b33fa7731939d9d996d1d0c9ff69c
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-05-19 16:06:38 +0100

    product card changes

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,10 +7,10 @@ const BestSeller = () => {
   return (
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Mais Vendidos</p>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-6 mt-6'>
         {products
           .filter(product => product.inStock)
-          .slice(0, 5)
+          .slice(0, 6)
           .map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
