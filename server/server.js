d89e381e4365e01551f800d0f56c5191eaf3455d
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-06-19 23:08:45 +0100

    server.js updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'elitesurfing.vercel.app'];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://elitesurfing.vercel.app',
+];
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
