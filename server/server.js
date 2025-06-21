a251ceff206463c1ffefcfe9cc56ce8f6d0a7bf4
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-06-21 07:41:52 +0100

    appContext updated

--- server/server.js
+++ server/server.js
@@ -31,17 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // permitir requisições sem origin (ex: Postman)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ['http://localhost:5173', 'https://elitesurfing.vercel.app'],
     credentials: true,
   })
 );
