617b0a931ff15a2a9f9eadc8a9c29d0c68f941d2
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-06-22 08:35:33 +0100

    updated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
       <Toaster />
       <ScrollToTop />
       <div
-        className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}
+        className={`${isSellerPath ? '' : 'px-4 md:px-16 lg:px-24 xl:px-32'}`}
       >
         <Routes>
           <Route path='/' element={<Home />} />
