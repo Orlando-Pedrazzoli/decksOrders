9d5aa3595659a70196b0286be3ebe0485584fea2
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-06-24 16:18:35 +0100

    userController updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,10 +25,10 @@ export const register = async (req, res) => {
     });
 
     res.cookie('token', token, {
-      httpOnly: true, // Prevent JavaScript to access cookie
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
-      maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
+      httpOnly: true,
+      secure: true, // Sempre true se usar HTTPS
+      sameSite: 'None', // Crucial para cookies entre domínios (frontend + backend separados)
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return res.json({
@@ -69,8 +69,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -102,9 +102,10 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie('token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,
+      sameSite: 'None',
     });
+
     return res.json({ success: true, message: 'Logged Out' });
   } catch (error) {
     console.log(error.message);
