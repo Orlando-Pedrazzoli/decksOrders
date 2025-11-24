29013ec2c04b3a0dc323d4f5fd55689356afb481
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-11-24 20:38:10 +0000

    hero image updated

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import { assets } from '../assets/assets';
 import { Link } from 'react-router-dom';
-import bannerVideo from '../assets/video.mp4';
-import pastori_video from '../assets/pastori_video.mp4';
 
 const MainBanner = () => {
   return (
     <div className='relative'>
-      {/* Vídeo para desktop */}
-      <video
-        src={bannerVideo}
-        autoPlay
-        loop
-        muted
-        playsInline
+      {/* Imagem Hero para desktop */}
+      <img
+        src='/hero-new.jpg'
+        alt='Elite Surfing - Surf Hard, Make History'
         className='w-full h-[500px] hidden md:block object-cover rounded-xl'
       />
 
-      {/* Vídeo para mobile */}
-      <video
-        src={pastori_video}
-        autoPlay
-        loop
-        muted
-        playsInline
+      {/* Imagem Hero para mobile */}
+      <img
+        src='/hero-new.jpg'
+        alt='Elite Surfing - Surf Hard, Make History'
         className='w-full h-[450px] md:hidden object-cover rounded'
-        style={{ aspectRatio: '9/16' }}
+        style={{ objectPosition: 'center' }}
       />
+
+      {/* Overlay escuro para melhor legibilidade do texto */}
+      <div className='absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent rounded-xl md:rounded-xl' />
 
       {/* Conteúdo sobreposto */}
       <div className='text-white absolute inset-0 flex flex-col items-start justify-end pb-16 md:pb-20 px-4 md:px-8 lg:px-12'>
